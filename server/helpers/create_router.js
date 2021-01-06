146428b96dd529cb9eb89f688b140e8848232e2a
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-06 14:17:00 +0000

    added some more routes to create_router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ReplSet } = require('mongodb');
 
 const createRouter = function (collection) {
 
@@ -12,10 +13,37 @@ const createRouter = function (collection) {
     // find all data in the collection (the documents), convert 
     // it to an array
     collection.find().toArray().then((docs ) => {
-      response.send(docs);
+      // response.send(docs); // send the data as is - it is in JSON format...
+      response.json(docs); // convert to JSON and send
     });
 
   });
+
+
+
+  // If you don't send a response to a request, then the client side will hang - indefinitely?
+
+
+  // Show route
+  // GET /:id
+  router.get("/:id", (req, res) => {
+    res.send("Placeholder!");
+  });
+
+
+  // Create route
+  // POST /
+  router.post("/", (req, res) => {
+    res.send("placeholder post response");
+  })
+
+  // Update route - could use PUT, PATCH, or even POST
+  // PUT /:id
+  router.put("/:id", (req, res) => {
+    res.send("placeholder for put.")
+  });
+
+
 
   return router;
 
